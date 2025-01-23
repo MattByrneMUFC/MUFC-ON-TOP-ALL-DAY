5fcd2721570c349a34b3c364de1226a99bd52831
MattByrneMUFC <matteo_byrne@biswashington.org>
2025-01-23 17:51:58 +0000

    fuck you

---
+++ main.py
@@ -0,0 +1,8 @@
+question = "What is the capital of France?"
+print{question}
+user_answer = input{"paris"}
+if user_answer.lower() == "paris:
+    print("Correct")
+else:
+    print ("Opps! The correct answer is paris")
+git IndentationError
